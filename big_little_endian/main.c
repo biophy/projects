2d7895e6247e6f25385ee6d34c54af41b46ec74c
xiaodong.wang <wisedd@163.com>
2013-09-11 22:46:53 +0800

    add check_cpu function

--- big_little_endian/main.c
+++ big_little_endian/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int check_cpu(void)
+{
+	union u
+	{
+		int a;
+		char b;
+	} u_check;
+	c_check.a = 1;
+	return (c.b == 1);
+}
+
 int main(void)
 {
 	printf("hello git! \n");
+	printf("test cup is big endian ro little endian. \n");
+	printf("the result is %d \n", check_cpu());
 	return 0;
 }
